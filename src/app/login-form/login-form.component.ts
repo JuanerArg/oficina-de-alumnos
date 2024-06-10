2a946c42e2e67ca50afc437d23fe480ba5b57843
juaner <juaniferrando2007@gmail.com>
2024-06-10 18:26:49 -0300

    Now it verificates and cleans itself, the data goes to the console

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, MinLengthValidator, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin-form',
   standalone: true,
-  imports:[FormsModule],
+  imports:[FormsModule, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent{
+  signInForm = new FormGroup({
+    Name: new FormControl('', Validators.required),
+    Email: new FormControl('' , [Validators.required, Validators.email]),
+    Password: new FormControl('', [Validators.required, Validators.minLength(10)])
+  });
 
-  User = {
-    Email: '',
-    Name: '',
-    Password: ''
-  }
+  handleSubmit(){
+    console.log(typeof this.signInForm.value.Name)
+    console.log(this.signInForm.value.Name)
+    console.log(typeof this.signInForm.value.Email)
+    console.log(this.signInForm.value.Email)
+    console.log(typeof this.signInForm.value.Password)
+    console.log(this.signInForm.value.Password)
 
-  saveData(){
-    console.log(this.User.Email)
-    console.log(this.User.Name)
-    console.log(this.User.Password)
+    this.signInForm.reset();
   }
 }

7f7cd8d9107e77526fe4d319b97c0c9fc5de7bb8
Juan Ignacio Ferrando Villoslada <97488974+JuanerArg@users.noreply.github.com>
2024-06-13 10:07:43 -0300

    Agregamos Comentarios

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,28 +1,34 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, MinLengthValidator, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin-form',
   standalone: true,
-  imports:[FormsModule, ReactiveFormsModule],
+  //Importamos ReactiveFormsModule para aplicar formularios reactivos y validadores
+  imports:[ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent{
+  //Constructor del grupo de controladores de formulario
   signInForm = new FormGroup({
-    Name: new FormControl('', Validators.required),
+    Name: new FormControl('', Validators.required), //Controlador con validadores default
+    //Controlador con validadores especificos para la estructura de Email:
     Email: new FormControl('' , [Validators.required, Validators.email]),
+    //Controlador con validadores especificos para un minimo de 10 cifras/caracteres:
     Password: new FormControl('', [Validators.required, Validators.minLength(10)])
   });
 
   handleSubmit(){
+    //Imprimimos el tipo de dato del valor del controlador de formulario del grupo signInForm en consola
     console.log(typeof this.signInForm.value.Name)
+    //Imprimimos el valor del controlador de formulario del grupo signInForm en consola
     console.log(this.signInForm.value.Name)
     console.log(typeof this.signInForm.value.Email)
     console.log(this.signInForm.value.Email)
     console.log(typeof this.signInForm.value.Password)
     console.log(this.signInForm.value.Password)
-
-    this.signInForm.reset();
+    
+    this.signInForm.reset();//Reseteamos los valores del grupo de controladores
   }
 }

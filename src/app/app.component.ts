64fcce10789ccd484cb107686cac3e693c7b76f0
juaner <juaniferrando2007@gmail.com>
2024-06-14 15:17:22 -0300

    Agregamos mainPage y enrutamiento basico (no termina de andar)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet ,RouterLink, RouterLinkActive } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginFormComponent],
+  imports: [RouterOutlet, LoginFormComponent, MainPageComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'MainPage', component: MainPageComponent },
+    { path: 'LoginForm', component: LoginFormComponent },
+];
